b2d8528fa2097036c52c94409a50777a95cbe39b
seabelis <seabelis74@gmail.com>
2019-10-16 17:56:44 +0200

    fixed import

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 // Import the model in your index.js.
-app.use(db)
+reauire(db)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
