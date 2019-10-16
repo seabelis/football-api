7e4fea923f1d3c7016d93e23d1ec249cf2e4ff5f
seabelis <seabelis74@gmail.com>
2019-10-16 20:49:10 +0200

    added Team model

---
+++ Team/model.js
@@ -0,0 +1,20 @@
+//Import Sequelize.
+const Sequelize = require('sequelize');
+
+// Import db.js as a constant named db.
+const db = require ('../db')
+
+// Declare a constant named Team. Capitalize the variable because it will be a class.
+// Set the variable equal to a call to db.define.
+const Team = db.define(
+//The second argument is an object that defines the table's fields. Add one string field called name and give it the type Sequelize.STRING.
+  'team',
+  {
+    name: {
+      type: Sequelize.STRING,
+      field: 'team_name'
+    }
+  }
+)
+//Export the model.
+module.exports = Team
--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 // Import the model in your index.js.
-reauire(db)
+const db = require ('./db')
+const teamModel= require ('./team/model')
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
