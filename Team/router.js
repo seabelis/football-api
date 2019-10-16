96ebda8ca96ad2be04ca8fbff8a1a7c4491285b5
seabelis <seabelis74@gmail.com>
2019-10-16 21:25:23 +0200

    fixed path

--- Team/router.js
+++ Team/router.js
@@ -1,1 +1,5 @@
 // Import the Router class from express.
+const { Router } = require('express');
+
+//Import the Team model class.
+const Team = require('./model');
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 4000
 // Import the model in your index.js.
 const db = require ('./db')
-const teamModel= require ('./team/model')
+const Team = require('./team/model');
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
