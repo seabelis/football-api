129abe38738e6e1d1f69afe5fca1eada01e57904
seabelis <seabelis74@gmail.com>
2019-10-16 21:57:28 +0200

    End Router section

--- Team/router.js
+++ Team/router.js
@@ -3,3 +3,23 @@ const { Router } = require('express');
 
 //Import the Team model class.
 const Team = require('./model');
+
+// Instantiate a router.
+const router = new Router();
+
+//Register a GET endpoint on the '/team' route. This route will get all the team rows from the table.
+//The route handler should take three arguments: the request (or req), the response (or res), and the next function.
+
+router.get('/team', (req, res, next) => {
+  // Inside the route handler: Call the Team.findAll method.
+  Team.findAll()
+  // Add a then callback. It will receive the list of teams. Send the list as the response.
+    .then(teams => {
+      res.send(teams);
+    })
+    // Add a catch callback. It will receive an error if it is thrown. Pass it to next.
+    .catch(next);
+});
+
+//Export the router.
+module.exports = router;
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
-// Import the model in your index.js.
-const db = require ('./db')
-const Team = require('./team/model');
+// Import ./team/router as a constant named teamRouter. 
+const teamRouter = require('./team/router');
+
+// You can remove the db and Team imports now, since they are already imported in team/router.js
+// const db = require ('./db')
+// const Team = require('./team/model');
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app
+  .use(teamRouter)
+
+  .listen(port, () => {
+    console.log(`App is listening on port ${port}`);
+  });
