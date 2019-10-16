0082d26bcc9675518a611977099ad348e83d4a23
seabelis <seabelis74@gmail.com>
2019-10-16 17:46:17 +0200

    End first part of Schema

---
+++ db.js
@@ -0,0 +1,21 @@
+// Import sequelize as a variabled named Sequelize. You should capitalize the variable because it is a class.
+const Sequelize = require('sequelize');
+
+// Declare a variable named databaseUrl and set it equal to your local database url, which should be 'postgres://postgres:<password>@localhost:5435/postgres', where <password> should be replaced with the password you've chosen for your database.
+const databaseUrl = 'postgres://postgres:secret@localhost:5435/postgres'
+
+// Create a new instance of the Sequelize class named db, passing the databaseUrl to the constructor.
+const db = new Sequelize('postgres://postgres:secret@localhost:5435/postgres')
+
+// Call the sync method of the instance you created. This method will sync the data in your database with the schema you are about to create.
+db
+  .sync()
+  // Add a then callback to sync that logs a message confirming the database schema has been updated.
+  .then(() => console.log('Database schema updated'))
+  // Add a catch callback that will pass any errors to console.error.
+  .catch(console.error)
+
+  // Export db.
+  module.exports = db
+
+
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
-
+// Import the model in your index.js.
+app.use(db)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
