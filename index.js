834ff8c43d3d3fd093a4a7ef092883640c88580c
seabelis <seabelis74@gmail.com>
2019-10-17 10:51:19 +0200

    Test deploy

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const port = process.env.PORT || 4000
 const teamRouter = require('./team/router');
 
 // You can remove the db and Team imports now, since they are already imported in team/router.js
-// const db = require ('./db')
+const db = require ('./db')
 // const Team = require('./team/model');
 
 
 app
   .use(teamRouter)
-
   .listen(port, () => {
     console.log(`App is listening on port ${port}`);
   });
