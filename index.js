2ea3387318626bb2dfcbb358e293e52a5c9455ab
seabelis <seabelis74@gmail.com>
2019-10-17 10:52:33 +0200

    Correct typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 // Import ./team/router as a constant named teamRouter. 
-const teamRouter = require('./team/router');
+const teamRouter = require('./Team/router');
 
 // You can remove the db and Team imports now, since they are already imported in team/router.js
 const db = require ('./db')
