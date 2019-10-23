5d756204098aef3a15c745890952716606ee468e
seabelis <seabelis74@gmail.com>
2019-10-23 12:24:22 +0200

    bcrypt working

--- user/router.js
+++ user/router.js
@@ -1,11 +1,16 @@
 const { Router } = require("express");
 const User = require("./model");
-
 const router = new Router();
+const bcrypt = require('bcrypt')
+
+
 
 router.post("/users", (req, res, next) => {
-  User.create(req.body)
-    .then(() => res.status(201).send({ message: "User created succesfully" }))
+  User.create({
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 10)
+  })
+    // .then(() => res.status(201).send({ message: "User created succesfully" }))
     .catch(next);
 });
 
